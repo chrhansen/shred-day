e8ac3247a87af4bcdb32deeb38cda91cce5c2a5e
Christian Hansen <christiandanmark@gmail.com>
2025-12-07 21:37:16 +0100

    Add Mission Control Jobs dashboard

---
+++ server/config/initializers/mission_control_jobs.rb
@@ -0,0 +1,9 @@
+user = ENV["MISSION_CONTROL_JOBS_USER"] || Rails.application.credentials.dig(:mission_control, :http_basic_auth_user)
+password = ENV["MISSION_CONTROL_JOBS_PASSWORD"] || Rails.application.credentials.dig(:mission_control, :http_basic_auth_password)
+
+if user.present? && password.present?
+  MissionControl::Jobs.http_basic_auth_user = user
+  MissionControl::Jobs.http_basic_auth_password = password
+else
+  Rails.logger.warn("Mission Control Jobs basic auth is not configured; dashboard will be inaccessible") if Rails.logger
+end
