c4aa370858dc7a558457e91f74b7240c0d37c8c1
Christian Hansen <christiandanmark@gmail.com>
2025-12-08 16:42:56 +0100

    Always use MissionControl base controller

--- server/config/initializers/mission_control_jobs.rb
+++ server/config/initializers/mission_control_jobs.rb
@@ -1,10 +1,11 @@
+MissionControl::Jobs.base_controller_class = "MissionControlJobsController"
+
 user = ENV["MISSION_CONTROL_JOBS_USER"] || Rails.application.credentials.dig(:mission_control, :http_basic_auth_user)
 password = ENV["MISSION_CONTROL_JOBS_PASSWORD"] || Rails.application.credentials.dig(:mission_control, :http_basic_auth_password)
 
 if user.present? && password.present?
   MissionControl::Jobs.http_basic_auth_user = user
   MissionControl::Jobs.http_basic_auth_password = password
-  MissionControl::Jobs.base_controller_class = "MissionControlJobsController"
 else
   Rails.logger.warn("Mission Control Jobs basic auth is not configured; dashboard will be inaccessible") if Rails.logger
 end
