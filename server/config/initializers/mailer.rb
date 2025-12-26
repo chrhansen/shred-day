23d95aa0cfd459ae2042b0970e485c4bfb3932c1
Christian Hansen <christiandanmark@gmail.com>
2025-12-26 16:39:33 +0100

    Set Resend.api_key in mailer initializer

--- server/config/initializers/mailer.rb
+++ server/config/initializers/mailer.rb
@@ -1,3 +1,4 @@
-Rails.application.config.action_mailer.resend_settings = {
-  api_key: Rails.application.credentials.dig(:resend, :api_key)
-}
+api_key = Rails.application.credentials.dig(:resend, :api_key)
+
+Resend.api_key = api_key
+Rails.application.config.action_mailer.resend_settings = { api_key: api_key }
